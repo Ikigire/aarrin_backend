4e3ef38f3391f0cc6758089f07b4a11e91cfbb41
Yael Alejandro Santana Michel <51036254+Ikigire@users.noreply.github.com>
2020-05-01 17:59:29 -0500

    Login Complete

--- aarrindb.sql
+++ aarrindb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-04-2020 a las 09:53:44
+-- Tiempo de generación: 01-05-2020 a las 17:47:10
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.11
 
@@ -25,6 +25,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `applications`
+--
+
+CREATE TABLE `applications` (
+  `IdApp` int(11) NOT NULL,
+  `IdCompany` int(11) NOT NULL,
+  `IdService` int(11) NOT NULL,
+  `AppLenguage` int(11) NOT NULL,
+  `LastCertificateExpiration` date DEFAULT NULL,
+  `LastCertificateCertifier` varchar(250) DEFAULT NULL,
+  `LastCertificateResults` varchar(1500) DEFAULT NULL,
+  `NumberEmployees` int(11) NOT NULL,
+  `ExternalServicesProvider` varchar(500) DEFAULT NULL,
+  `ReceiveConsultancy` tinyint(1) DEFAULT NULL,
+  `ConsultantName` varchar(500) DEFAULT NULL,
+  `AppDate` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `app_detail`
+--
+
+CREATE TABLE `app_detail` (
+  `IdAppDetail` int(11) NOT NULL,
+  `IdApp` int(11) NOT NULL,
+  `Address` varchar(500) NOT NULL,
+  `ShiftEmployees1` int(11) NOT NULL,
+  `ShiftEmployees2` int(11) NOT NULL,
+  `ShiftEmployees3` int(11) NOT NULL,
+  `Activities` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `companies`
 --
 
@@ -35,16 +72,17 @@ CREATE TABLE `companies` (
   `CompanyRFC` varchar(20) NOT NULL,
   `CompanyAddress` varchar(500) NOT NULL,
   `CompanyWebsite` varchar(250) DEFAULT NULL,
-  `CompanyPassword` varbinary(20) NOT NULL
+  `CompanyPassword` varbinary(20) NOT NULL,
+  `CompanyLogo` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `companies`
 --
 
-INSERT INTO `companies` (`IdCompany`, `IdSector`, `CompanyName`, `CompanyRFC`, `CompanyAddress`, `CompanyWebsite`, `CompanyPassword`) VALUES
-(2, 1001, 'Prueba S.A de C.V.', 'PRUEBA7845', 'Ramón Corona #125', 'https://www.prueba.com.mx', 0x5411a461e39fe28709a073078ffc3e42),
-(3, 1015, 'Malvados y Asociados S.A de C.V.', 'MALASO4589', 'Independencia #425', 'https://malvados.asociados.mx', 0xdf3e0d5bbde5b0962b47b8b6cca37428);
+INSERT INTO `companies` (`IdCompany`, `IdSector`, `CompanyName`, `CompanyRFC`, `CompanyAddress`, `CompanyWebsite`, `CompanyPassword`, `CompanyLogo`) VALUES
+(2, 1001, 'Prueba S.A de C.V.', 'PRUEBA7845', 'Ramón Corona #125', 'https://www.prueba.com.mx', 0x5411a461e39fe28709a073078ffc3e42, NULL),
+(3, 1015, 'Malvados y Asociados S.A de C.V.', 'MALASO4589', 'Independencia #425', 'https://malvados.asociados.mx', 0xdf3e0d5bbde5b0962b47b8b6cca37428, NULL);
 
 -- --------------------------------------------------------
 
@@ -63,20 +101,73 @@ CREATE TABLE `contacts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Disparadores `contacts`
+-- Volcado de datos para la tabla `contacts`
 --
-DELIMITER $$
-CREATE TRIGGER `unique_main_contact_insert` AFTER INSERT ON `contacts` FOR EACH ROW IF (NEW.MainContact = 1) THEN BEGIN
-UPDATE contacts SET MainContact = 0 WHERE IdCompany = NEW.IdCompany AND IdContact <> NEW.IdContact;
-END; END IF
-$$
-DELIMITER ;
-DELIMITER $$
-CREATE TRIGGER `unique_main_contact_update` AFTER UPDATE ON `contacts` FOR EACH ROW IF(NEW.MainContact = 1) THEN BEGIN
-UPDATE contacts SET MainContact = 0 WHERE IdCompany = NEW.IdCompany AND IdContact <> NEW.IdContact;
-END; END IF
-$$
-DELIMITER ;
+
+INSERT INTO `contacts` (`IdContact`, `IdCompany`, `MainContact`, `ContactName`, `ContactPhone`, `ContactEmail`, `ContactCharge`) VALUES
+(11, 3, 1, 'Heinz Doofenzmirt', '5555555555', 'evil_doof@mermelada.com', 'Genio malvado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `master_list`
+--
+
+CREATE TABLE `master_list` (
+  `IdMasterList` int(11) NOT NULL,
+  `IdService` int(11) NOT NULL,
+  `IdSector` int(11) NOT NULL,
+  `IdEmployee` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personal`
+--
+
+CREATE TABLE `personal` (
+  `IdEmployee` int(11) NOT NULL,
+  `EmployeeName` varchar(75) NOT NULL,
+  `EmployeeLastName` varchar(75) NOT NULL,
+  `EmployeeDegree` varchar(200) NOT NULL,
+  `EmployeeBirth` date NOT NULL,
+  `EmployeeContractYear` year(4) NOT NULL,
+  `EmployeeCharge` varchar(150) NOT NULL,
+  `EmployeeAddress` varchar(500) NOT NULL,
+  `EmployeePhone` varchar(25) NOT NULL,
+  `EmployeeEmail` varchar(200) NOT NULL,
+  `EmployeeInsurance` varchar(30) NOT NULL,
+  `EmployeeRFC` varchar(25) NOT NULL,
+  `EmployeePassword` varbinary(20) NOT NULL,
+  `EmployeePhoto` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `personal`
+--
+
+INSERT INTO `personal` (`IdEmployee`, `EmployeeName`, `EmployeeLastName`, `EmployeeDegree`, `EmployeeBirth`, `EmployeeContractYear`, `EmployeeCharge`, `EmployeeAddress`, `EmployeePhone`, `EmployeeEmail`, `EmployeeInsurance`, `EmployeeRFC`, `EmployeePassword`, `EmployeePhoto`) VALUES
+(1, 'Adrian', 'Casillas', 'Lic. Informático', '1985-01-01', 2000, 'Representante LATAM', 'Domicilio Particular #12', '5555555555', 'ya_el1995@hotmail.com', 'KIAS5849a8', 'CASA850101PL5', 0x066cff26d5e35df5ee74a9f26ff64f93, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `IdRole` int(11) NOT NULL,
+  `IdEmployee` int(11) NOT NULL,
+  `Role_Type` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`IdRole`, `IdEmployee`, `Role_Type`) VALUES
+(100, 1, 'ADMIN_ROLE');
 
 -- --------------------------------------------------------
 
@@ -85,7 +176,7 @@ DELIMITER ;
 --
 
 CREATE TABLE `sectors` (
-  `IdSector` int(10) NOT NULL,
+  `IdSector` int(11) NOT NULL,
   `SectorName` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -137,6 +228,17 @@ INSERT INTO `sectors` (`IdSector`, `SectorName`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `services`
+--
+
+CREATE TABLE `services` (
+  `IdService` int(11) NOT NULL,
+  `ServiceStandard` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `validation_keys`
 --
 
@@ -159,10 +261,24 @@ INSERT INTO `validation_keys` (`IdKey`, `ValidationCode`, `ValidationDate`, `Val
 --
 
 --
+-- Indices de la tabla `applications`
+--
+ALTER TABLE `applications`
+  ADD PRIMARY KEY (`IdApp`),
+  ADD KEY `fk_services` (`IdService`);
+
+--
+-- Indices de la tabla `app_detail`
+--
+ALTER TABLE `app_detail`
+  ADD PRIMARY KEY (`IdAppDetail`);
+
+--
 -- Indices de la tabla `companies`
 --
 ALTER TABLE `companies`
   ADD PRIMARY KEY (`IdCompany`),
+  ADD UNIQUE KEY `CompanyRFC` (`CompanyRFC`),
   ADD KEY `fk_sectors` (`IdSector`);
 
 --
@@ -173,10 +289,36 @@ ALTER TABLE `contacts`
   ADD KEY `fk_companies` (`IdCompany`);
 
 --
+-- Indices de la tabla `master_list`
+--
+ALTER TABLE `master_list`
+  ADD PRIMARY KEY (`IdMasterList`);
+
+--
+-- Indices de la tabla `personal`
+--
+ALTER TABLE `personal`
+  ADD PRIMARY KEY (`IdEmployee`),
+  ADD UNIQUE KEY `unique_email` (`EmployeeEmail`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`IdRole`),
+  ADD KEY `fk_personal` (`IdEmployee`);
+
+--
 -- Indices de la tabla `sectors`
 --
 ALTER TABLE `sectors`
   ADD PRIMARY KEY (`IdSector`);
+
+--
+-- Indices de la tabla `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`IdService`);
 
 --
 -- Indices de la tabla `validation_keys`
@@ -189,6 +331,18 @@ ALTER TABLE `validation_keys`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `applications`
+--
+ALTER TABLE `applications`
+  MODIFY `IdApp` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `app_detail`
+--
+ALTER TABLE `app_detail`
+  MODIFY `IdAppDetail` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `companies`
 --
 ALTER TABLE `companies`
@@ -198,13 +352,37 @@ ALTER TABLE `companies`
 -- AUTO_INCREMENT de la tabla `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `IdContact` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdContact` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `master_list`
+--
+ALTER TABLE `master_list`
+  MODIFY `IdMasterList` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `personal`
+--
+ALTER TABLE `personal`
+  MODIFY `IdEmployee` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `IdRole` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- AUTO_INCREMENT de la tabla `sectors`
 --
 ALTER TABLE `sectors`
-  MODIFY `IdSector` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1043;
+  MODIFY `IdSector` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1043;
+
+--
+-- AUTO_INCREMENT de la tabla `services`
+--
+ALTER TABLE `services`
+  MODIFY `IdService` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `validation_keys`
@@ -217,6 +395,12 @@ ALTER TABLE `validation_keys`
 --
 
 --
+-- Filtros para la tabla `applications`
+--
+ALTER TABLE `applications`
+  ADD CONSTRAINT `fk_services` FOREIGN KEY (`IdService`) REFERENCES `services` (`IdService`) ON UPDATE NO ACTION;
+
+--
 -- Filtros para la tabla `companies`
 --
 ALTER TABLE `companies`
@@ -227,6 +411,12 @@ ALTER TABLE `companies`
 --
 ALTER TABLE `contacts`
   ADD CONSTRAINT `fk_companies` FOREIGN KEY (`IdCompany`) REFERENCES `companies` (`IdCompany`) ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD CONSTRAINT `fk_personal` FOREIGN KEY (`IdEmployee`) REFERENCES `personal` (`IdEmployee`) ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
