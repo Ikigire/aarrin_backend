0f3a2bd93071ee022783d4a489f66019d7268299
Yael Alejandro Santana Michel <51036254+Ikigire@users.noreply.github.com>
2020-05-29 22:22:39 -0500

    Update Connection.php

--- Config/Connection.php
+++ Config/Connection.php
@@ -11,6 +11,9 @@
     $dsn = "mysql:dbname=$dataBase;host=$host";
     try{
     $dbConnection = new PDO($dsn, $userName, $password);
+    $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $dbConnection->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     $dbConnection->query("SET NAMES 'utf8'");//formating data to utf-8
     } catch (PDOException $e){
