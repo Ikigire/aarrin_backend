b4e9ceac5bb8d2be19bcdf4f26e8d15bb38caabf
Hander <56362981+tenien@users.noreply.github.com>
2021-01-13 23:15:01 -0600

    correciones
    * errores en el push y en el post
    corregidas

--- controller/tech_report.controller.php
+++ controller/tech_report.controller.php
@@ -26,7 +26,7 @@ switch ($url[5]) {
 
         if (TokenTool::isValid($token)) {
             
-            $query = "SELECT idTechReport, idQuestion, st1, st1compliance, st2, st2compliance, surv1, surv1compliance, surv2, surv2compliance FROM tech_report WHERE IdAuditPlan = :idAuditPlan";
+            $query = "SELECT idTechReport, idQuestion, st1, st1compliance, st2, st2compliance, surv1, surv1compliance, surv2, surv2compliance FROM tech_report WHERE idAuditPlan = :idAuditPlan";
             $data = DBManager::query($query, array(':idAuditPlan' => $idAuditPlan));
 
             if ($data) {
@@ -68,13 +68,14 @@ switch ($url[5]) {
         }
 
         if (TokenTool::isValid($token)) {
-            if (isset($data['IdAuditPlan'])) {
-
-                $initialPart = "INSERT INTO tech_report(idTechReport, IdAuditPlan, st1, st1compliance, st2, st2compliance, surv1, surv1compliance, surv2, surv2compliance";
-                $values = "VALUES (Null, :idAuditPlan, :st1, :st1compliance, :st2, :st2compliance, :surv1, :surv1compliance, :surv2, :surv2compliance";
+            if (isset($data['idAuditPlan'])) {
+                
+                $initialPart = "INSERT INTO tech_report(idTechReport, idAuditPlan, idQuestion, st1, st1compliance, st2, st2compliance, surv1, surv1compliance, surv2, surv2compliance";
+                $values = "VALUES (null, :idAuditPlan, :idQuestion , :st1, :st1compliance, :st2, :st2compliance, :surv1, :surv1compliance, :surv2, :surv2compliance";
 
                 $params = array(
                     ':idAuditPlan'     => $data['idAuditPlan'],
+                    ':idQuestion'      => $data['idQuestion'],
                     ':st1'             => $data['st1'],
                     ':st1compliance'   => (int) $data['st1compliance'],
                     ':st2'             => $data['st2'],
@@ -84,20 +85,10 @@ switch ($url[5]) {
                     ':surv2'           => $data['surv2'],
                     ':surv2compliance' => (int) $data['surv2compliance'],
                 );
-
                 $query = $initialPart .")" .$values .");";
                 $response = DBManager::query($query, $params);
                 if ($response) {
                     header(HTTP_CODE_201);
-                    $query = "SELECT idTechReport, st1, st1compliance, st2, st2compliance, surv1, surv1compliance, surv2, surv2compliance FROM tech_report WHERE IdAuditPlan: idAuditPlan";
-                    $data = DBManager::query($query, array(':idAuditPlan' => $idAuditPlan));
-                    for ($i=0; $i < count($data); $i++) { 
-                        $data[$i]['idTechReport']    = (int)   $data[$i]['idTechReport'];
-                        $data[$i]['st1compliance']   = (bool)  $data[$i]['st1compliance'];
-                        $data[$i]['st2compliance']   = (bool)  $data[$i]['st2compliance'];
-                        $data[$i]['surv1compliance'] = (bool)  $data[$i]['surv1compliance'];
-                        $data[$i]['surv2compliance'] = (bool)  $data[$i]['surv2compliance'];
-                    }
                     echo json_encode($data);
                 } else {
                     header(HTTP_CODE_409);
@@ -147,12 +138,12 @@ switch ($url[5]) {
                     }
 
                     if (isset($data['st1compliance'])) {
-                        $params[':st1compliance'] = $data['st1compliance'];
+                        $params[':st1compliance'] = (int) $data['st1compliance'];
                         $query .= ", st1compliance = :st1compliance";
                     }
 
                     if (isset($data['st2compliance'])) {
-                        $params[':st2compliance'] = $data['st2compliance'];
+                        $params[':st2compliance'] = (int) $data['st2compliance'];
                         $query .= ", st2compliance = :st2compliance";
                     }
 
@@ -167,12 +158,12 @@ switch ($url[5]) {
                     }
 
                     if (isset($data['surv1compliance'])) {
-                        $params[':surv1compliance'] = $data['surv1compliance'];
+                        $params[':surv1compliance'] = (int) $data['surv1compliance'];
                         $query .= ", surv1compliance = :surv1compliance";
                     }
 
                     if (isset($data['surv2compliance'])) {
-                        $params[':surv2compliance'] = $data['surv2compliance'];
+                        $params[':surv2compliance'] = (int) $data['surv2compliance'];
                         $query .= ", surv2compliance = :surv2compliance";
                     }
     
